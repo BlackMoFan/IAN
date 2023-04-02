9494428cec4bedfee0a30db8b3fa1807f9636ea5
BlackMoFan <rodlester.moreno23@gmail.com>
2023-04-03 06:21:13 +0800

    ignoreBuildErrors

--- components/MainPart.tsx
+++ components/MainPart.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import axios from "axios";
 import { useDropzone } from 'react-dropzone';
 import {useCallback} from 'react';
