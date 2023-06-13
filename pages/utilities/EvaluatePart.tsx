959fbc5d577e81d41fe244cc46c73aee319fa004
BlackMoFan <rodlester.moreno23@gmail.com>
2023-06-13 14:15:47 +0800

    2nd iteration: fixed styling

--- pages/utilities/EvaluatePart.tsx
+++ pages/utilities/EvaluatePart.tsx
@@ -5,28 +5,28 @@ const EvaluatePart= () => {
 
     return (
         <div className="grid justify-center h-screen grid-cols-12 gap-6 p-4 px-5 bg-white lg:px-48 flex-center place-items-center">
-            <div className="grid w-full h-full col-span-12 p-4 text-center mb-9 lg:h-full bg-input-area place-items-center text-input-area-text md:col-span-6 lg:col-span-7 rounded-2xl">
+            <div className="grid w-full h-full col-span-12 p-4 text-center mb-9 lg:h-full bg-input-area place-items-center text-input-area-text sm:col-span-6 lg:col-span-7 rounded-2xl">
                 <img 
                     className="w-full max-w-xl transition rounded-md max-h-fit hover:border-gray-400 focus:outline-none"
                     src="/classified2.png" 
                     alt=""
                 />
             </div>
-            <div className="h-full col-span-12 p-3 text-black lg:h-[100vh] lg:col-span-5 md:col-span-6 rounded-2xl place-content-center">
+            <div className="h-full col-span-12 p-3 text-black lg:h-[100vh] lg:col-span-5 sm:col-span-6 rounded-2xl place-content-center">
                 <div className="flex flex-row gap-6 my-6 place-items-center">
-                    <svg fill="#9F8FB4" className="w-20 h-20" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 94 94">
-                    <g>
-                        <path d="M94,88c0,3.312-2.688,6-6,6H6c-3.314,0-6-2.688-6-6V6c0-3.313,2.686-6,6-6h82c3.312,0,6,2.687,6,6V88z"/>
-                    </g>
-                    </svg>
+                    <img
+                        className="w-20 h-20 max-w-xl transition rounded-md max-h-fit hover:border-gray-400 focus:outline-none"
+                        src="m3.svg"
+                        alt=""
+                    />
                     <h2 className="text-2xl">Mandibular Third Molar</h2>
                 </div>
                 <div className="flex flex-row gap-6 my-6 place-items-center">
-                    <svg fill="#894A68" className="w-20 h-20" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 94 94">
-                    <g>
-                        <path d="M94,88c0,3.312-2.688,6-6,6H6c-3.314,0-6-2.688-6-6V6c0-3.313,2.686-6,6-6h82c3.312,0,6,2.687,6,6V88z"/>
-                    </g>
-                    </svg>
+                    <img
+                        className="w-20 h-20 max-w-xl transition rounded-md max-h-fit hover:border-gray-400 focus:outline-none"
+                        src="mc.svg"
+                        alt=""
+                    />
                     <h2 className="text-2xl">Mandibular Canal</h2>
                 </div>
                 <h1 className="text-4xl font-bold my-9">Results</h1>
