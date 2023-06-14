d89156945d5828ed5c666e131ddf78ed8c540019
BlackMoFan <rodlester.moreno23@gmail.com>
2023-06-14 13:56:19 +0800

    3rd iteration: fixed styling

--- pages/utilities/EvaluatePart.tsx
+++ pages/utilities/EvaluatePart.tsx
@@ -13,10 +13,10 @@ const EvaluatePart= () => {
                         alt=""
                     />
                 </div>
-                <div className="h-full col-span-12 lg:mt-9 p-3 text-black lg:h-[100vh] lg:col-span-5 sm:col-span-6 rounded-2xl place-content-center">
-                    <div className="flex flex-row gap-6 my-6 place-items-center">
+                <div className="h-full col-span-12 p-3 text-black lg:h-[100vh] lg:col-span-5 sm:col-span-6 rounded-2xl place-content-center">
+                    <div className="flex flex-row gap-6 my-6 lg:mt-24 place-items-center">
                         <img
-                            className="max-w-xl transition rounded-md md:w-10 md:h-10 lg:w-20 lg:h-20 max-h-fit hover:border-gray-400 focus:outline-none"
+                            className="w-10 h-10 max-w-xl transition rounded-md lg:w-20 lg:h-20 max-h-fit hover:border-gray-400 focus:outline-none"
                             src="m3.svg"
                             alt=""
                         />
@@ -24,7 +24,7 @@ const EvaluatePart= () => {
                     </div>
                     <div className="flex flex-row gap-6 my-6 place-items-center">
                         <img
-                            className="max-w-xl transition rounded-md md:w-10 md:h-10 lg:w-20 lg:h-20 max-h-fit hover:border-gray-400 focus:outline-none"
+                            className="w-10 h-10 max-w-xl transition rounded-md lg:w-20 lg:h-20 max-h-fit hover:border-gray-400 focus:outline-none"
                             src="mc.svg"
                             alt=""
                         />
